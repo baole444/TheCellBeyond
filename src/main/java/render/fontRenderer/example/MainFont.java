edb617e1ada43e588bd8f15ea06946c92a2b4c84
Baole444 <nguyenbaole246@gmail.com>
2025-04-20 12:49:54 +0700

    Rollback text rendering

--- src/main/java/render/fontRenderer/example/MainFont.java
+++
@@ -1,9 +0,0 @@
-package render.fontRenderer.example;
-
-public class MainFont {
-    public static void main(String[] args) {
-        WindowFont window = new WindowFont();
-
-        window.run();
-    }
-}
