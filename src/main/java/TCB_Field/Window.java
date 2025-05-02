89bbf91b027660d1853f556f4afe368c493211bf
Baole444 <nguyenbaole246@gmail.com>
2025-05-02 08:06:15 +0700

    Fix missing texture generation in main loop

--- src/main/java/TCB_Field/Window.java
+++ src/main/java/TCB_Field/Window.java
@@ -24,6 +24,7 @@ import org.lwjgl.opengl.GL;
 import physic_2d.FlatPhysic;
 import render.*;
 import render.Renderer;
+import render.text.FontManager;
 import scene.LevelEditorSceneInit;
 import scene.Scene;
 import scene.SceneInit;
@@ -362,6 +363,8 @@ public class Window implements IEvent {
             glfwPollEvents(); //poll events
 
             if (dt >= 0) {
+                FontManager.get().updateFontTextures();
+
                 // Pass 1: object selection layer (invisible)
 
                 rendererState.setRenderPass(RendererState.RenderPass.SELECTION);
