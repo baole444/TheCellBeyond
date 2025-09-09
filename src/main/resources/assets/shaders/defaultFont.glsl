c55a715c8b9c84225bfd3e7950bf0ee703811912
Baole444 <nguyenbaole246@gmail.com>
2025-09-09 22:15:44 +0700

    Prototyping with MSDFGen and FreeType
    - Refactoring TCBFont to use msdfgen binding and freetype for new atlas generation with multi-channel SDF font for better AA quality.

--- src/main/resources/assets/shaders/defaultFont.glsl
+++ src/main/resources/assets/shaders/defaultFont.glsl
@@ -24,11 +24,26 @@ in vec4 fColor;
 in vec2 fTexCrd;
 
 uniform sampler2D uFontTex;
+uniform float uPxRange = 4.0;
 
 out vec4 color;
 
+float median(float r, float g, float b) {
+    return max(min(r, g), min(max(r, g), b));
+}
+
+float screenPxRange() {
+    vec2 unitRange = vec2(uPxRange) / vec2(textureSize(uFontTex, 0));
+    vec2 screenTexSize = vec2(1.0) / fwidth(fTexCrd);
+    return max(0.5 * dot(unitRange, screenTexSize), 1.0);
+}
+
 void main()
 {
-    float alpha = texture(uFontTex, fTexCrd).r;
+    vec3 msd = texture(uFontTex, fTexCrd).rgb;
+    float sd = median(msd.r, msd.g, msd.b);
+    float screenPxDistance = screenPxRange() * (sd - 0.5);
+    float alpha = clamp(screenPxDistance + 0.5, 0.0, 1.0);
+    alpha = smoothstep(0.3, 0.7, alpha);
     color = vec4(fColor.rgb, fColor.a * alpha);
 }
