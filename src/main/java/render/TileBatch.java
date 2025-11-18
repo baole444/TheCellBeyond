c9d71ca40511dac6902b45565d50e6df27614b91
Baole444 <nguyenbaole246@gmail.com>
2025-11-19 02:26:09 +0700

    Minor bug fix for Tilebatch(2)
    - Fix inverted Y axis for TileBatch when calculating tile position

--- src/main/java/render/TileBatch.java
+++ src/main/java/render/TileBatch.java
@@ -137,7 +137,7 @@ public class TileBatch implements Comparable<TileBatch> {
 
     private Vector2f calculateTilePosition(Vector2f tileMapPosition, Vector2i mapCoordinate, Vector2f gridSizeWorld) {
         float x = tileMapPosition.x + mapCoordinate.x * gridSizeWorld.x + gridSizeWorld.x / 2.0f;
-        float y = tileMapPosition.y + mapCoordinate.y * gridSizeWorld.y + gridSizeWorld.y / 2.0f;
+        float y = tileMapPosition.y - mapCoordinate.y * gridSizeWorld.y + gridSizeWorld.y / 2.0f;
 
         return new Vector2f(x, y);
     }
@@ -151,10 +151,10 @@ public class TileBatch implements Comparable<TileBatch> {
         float yAdd = 0.5f;
 
         Vector2f[] corners = {
-                new Vector2f(position.x + size.x * xAdd, - position.y + size.y * yAdd),
-                new Vector2f(position.x + size.x * xAdd, - position.y - size.y * yAdd),
-                new Vector2f(position.x - size.x * xAdd, - position.y - size.y * yAdd),
-                new Vector2f(position.x - size.x * xAdd, - position.y + size.y * yAdd)
+                new Vector2f(position.x + size.x * xAdd, position.y + size.y * yAdd),
+                new Vector2f(position.x + size.x * xAdd, position.y - size.y * yAdd),
+                new Vector2f(position.x - size.x * xAdd, position.y - size.y * yAdd),
+                new Vector2f(position.x - size.x * xAdd, position.y + size.y * yAdd)
         };
 
         for (int i = 0; i < 4; i++) {
