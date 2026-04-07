ab312a0290e27c620c9f2fb415b5580cae2fed63
Baole444 <nguyenbaole246@gmail.com>
2026-04-07 07:22:33 +0700

    Add default type hint value for Export

--- src/main/java/scripting/Export.java
+++ src/main/java/scripting/Export.java
@@ -10,5 +10,5 @@ import java.lang.annotation.Target;
 public @interface Export {
     String label() default "";
     String description() default "";
-
+    TypeHint type() default TypeHint.Auto;
 }
